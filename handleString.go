fe5a07f79a1687e3a9321f3b97db28f7bcc4f313
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-12-26 15:56:57 -0800

    Learned struct

---
+++ handleString.go
@@ -0,0 +1,16 @@
+package main
+
+//func main() {
+//	s := "yes我爱慕课网!"
+//	fmt.Println("len(s) =", len(s))
+//
+//	for _, b := range []byte(s) {
+//		fmt.Printf("%X ", b)
+//	}
+//	fmt.Println()
+//
+//	for _, ch := range []rune(s) {
+//		fmt.Printf("%v ", ch)
+//	}
+//	fmt.Println()
+//}
